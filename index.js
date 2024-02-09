ff397735d6ffb182883e6acde5106f6692c64d08
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2024-02-09 12:14:09 +0600

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true,limit:52428800}))
 app.use('/',Router)
 
 
-app.listen(8000,()=>{
+app.listen(3000,()=>{
     console.log("Server connected succcessfull")
 })
 Connection()
