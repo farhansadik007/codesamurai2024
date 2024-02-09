becd2251443e878ac0b9f79fd1ac5fffe7b7136c
Sunan13208 <sunanrabbi1918@gmail.com>
2024-02-09 17:39:01 +0600

    wallet added

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { addUser } from "../Controller/UserController.js";
-import { addStaion,getAllStation } from "../Controller/StationController.js";
+import { addStaion, getAllStation } from "../Controller/StationController.js";
+import { addBalance, getWallet } from "../Controller/walletController.js";
 const Router = express.Router()
-Router.post("/api/users",addUser);
-Router.post("/api/stations",addStaion)
-Router.get("/api/stations",getAllStation);
-
-
+Router.post("/api/users", addUser);
+Router.post("/api/stations", addStaion)
+Router.get("/api/stations", getAllStation);
+Router.get("/api/wallets/:id", getWallet);
+Router.put("/api/wallets/:id", addBalance)
 
 export default Router
